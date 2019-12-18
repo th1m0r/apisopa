a3aa11e3803c109c15cd4e1764f0572108664ea0
Thiago Moreno <thiagocmoreno@gmail.com>
2019-12-18 13:46:27 -0300

    inclusao da timezone na string do banco de dados e na configuracao default do projeto

--- src/main/java/br/org/sopa/SopaApplication.java
+++ src/main/java/br/org/sopa/SopaApplication.java
@@ -1,12 +1,28 @@
 package br.org.sopa;
 
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SopaApplication {
 
+	private static ApplicationContext APPLICATION_CONTEXT;
+
 	public static void main(String[] args) {
-		SpringApplication.run(SopaApplication.class, args);
+		APPLICATION_CONTEXT = SpringApplication.run(SopaApplication.class, args);
+	}
+
+	public static <T> T getBean(Class<T> requiredType) {
+		return APPLICATION_CONTEXT.getBean(requiredType);
+	}
+
+	@PostConstruct
+	public void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("America/Recife"));
 	}
 }
